15357f686b8939d66dc63d43ef31cd26d365de44
bovar20 <shiningstarfilmsuk@hotmail.co.uk>
2018-08-13 18:39:08 +0100

    Enemy Health bar 75% done

--- Assets/Scripts/EnemyHealthManager.cs
+++ Assets/Scripts/EnemyHealthManager.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealthManager : MonoBehaviour {
 
+    public int maxEnemyHealth;
+
 	public int enemyHealth;
+
+    public GameObject enemyTide;
 
 	//public GameObject deathEffect;
 
 	public int pointsOnDeath;
 
+    public GameObject healthBar;
+
 	// Use this for initialization
 	void Start () {
-		
+
+        enemyHealth = maxEnemyHealth;
 	}
 	
 	// Update is called once per frame
@@ -21,8 +29,9 @@ public class EnemyHealthManager : MonoBehaviour {
 		{
 			//Instantiate (deathEffect, transform.position, transform.rotation);
 			ScoreManager.AddPoints (pointsOnDeath);
-			Destroy (gameObject);
+            Destroy (enemyTide);
 		}
+        healthBar.GetComponent<Slider>().value = enemyHealth;
 	}
 
 	public void giveDamage(int damageToGive)
---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour {
+
+	public int maxPlayerHealth;
+
+	public static int playerHealth;
+
+	//Text text;
+
+	public Slider healthBar;
+
+	//private LevelManager levelManager;
+
+	//public bool isDead;
+
+	//private LifeManager lifeSystem;
+
+	//private TimeManager theTime;
+
+	// Use this for initialization
+	void Start () {
+		//text = GetComponent<Text> ();
+
+		healthBar = GetComponent<Slider> ();
+
+		playerHealth = maxPlayerHealth;
+
+		//levelManager = FindObjectOfType<LevelManager> ();
+
+		//lifeSystem = FindObjectOfType<LifeManager> ();
+
+		//theTime = FindObjectOfType<TimeManager> ();
+
+		//isDead = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (playerHealth <= 0 && !isDead) 
+		//{
+			//playerHealth = 0;
+			//levelManager.RespawnPlayer ();
+			//lifeSystem.TakeLife ();
+			//isDead = true;
+
+		//}
+		if (playerHealth > maxPlayerHealth) {
+			playerHealth = maxPlayerHealth;
+		}
+
+
+		healthBar.value = playerHealth;
+		//text.text = "" + playerHealth;
+	}
+
+	public static void HurtPlayer(int damageToGive)
+	{
+		playerHealth -= damageToGive;
+	}
+
+	public void FullHealth()
+	{
+		playerHealth = maxPlayerHealth;	
+	}
+
+	public void KillPlayer(){
+		playerHealth = 0;
+	}
+}
